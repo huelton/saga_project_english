ca7823707341b93973c044e07db2d4805fbf9939
Huelton Santos <hueltondihon@gmail.com>
2026-03-06 08:27:04 -0300

    feat(currency-service): add pom and Redis/Kafka config

---
+++ currency-service/src/main/java/com/saga/currency/CurrencyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.saga.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyServiceApplication.class, args);
+    }
+}
